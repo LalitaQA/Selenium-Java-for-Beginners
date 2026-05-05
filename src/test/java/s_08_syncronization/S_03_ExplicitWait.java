f9a90eba4f6311ced2991356f45f407bde1a03b1
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-05 15:56:58 +0530

    push changes

---
+++ src/test/java/s_08_syncronization/S_03_ExplicitWait.java
@@ -0,0 +1,13 @@
+package s_08_syncronization;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class S_03_ExplicitWait {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+}
