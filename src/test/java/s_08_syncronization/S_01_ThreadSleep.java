a9d58493ed4f06d3888cca703af2ecdaf40fb97c
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-05 14:46:49 +0530

    push changes

--- src/test/java/s_08_syncronization/S_01_ThreadSleep.java
+++ src/test/java/s_08_syncronization/S_01_ThreadSleep.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class S_01_ThreadSleep {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://demowebshop.tricentis.com/");
-		
-		
-	}
 
+		Thread.sleep(3000);
+		driver.get("https://demowebshop.tricentis.com/");
+
+	}
 }
