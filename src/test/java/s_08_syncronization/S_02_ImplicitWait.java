d875f099c7f73db1bb36ad789ae101a6e21bffab
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-05 15:31:25 +0530

    push changes

--- src/test/java/s_08_syncronization/S_02_ImplicitWait.java
+++ src/test/java/s_08_syncronization/S_02_ImplicitWait.java
@@ -1,5 +1,6 @@
 package s_08_syncronization;
 
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,10 +10,12 @@ public class S_02_ImplicitWait {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 		driver.get("https://www.hyrtutorials.com/p/waits-demo.html");
 
 		driver.findElement(By.id("btn1")).click();
 
-		driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys("Hello!");
+		driver.findElement(By.cssSelector("[id='txt1']")).sendKeys("Hello!");
 	}
 }
