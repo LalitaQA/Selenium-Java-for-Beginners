43eb639b89b5cb2cbb918a282e50e07fc45ffeb3
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-06 15:11:40 +0530

    push changes

--- src/test/java/s_08_syncronization/S_03_ExplicitWait.java
+++ src/test/java/s_08_syncronization/S_03_ExplicitWait.java
@@ -3,17 +3,23 @@ package s_08_syncronization;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class S_03_ExplicitWait {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
+
 		driver.get("https://omayo.blogspot.com/");
-		
+
 		driver.findElement(By.className("dropbtn")).click();
-		
+
+		// explicit wait
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Facebook']")));
+
 		driver.findElement(By.xpath("//a[text()='Facebook']")).click();
 	}
 
