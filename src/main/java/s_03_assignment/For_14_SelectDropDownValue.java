4e464146b7ca7d0797e010f75af73513abe2179e
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-07 15:03:50 +0530

    push changes

--- src/main/java/s_03_assignment/For_14_SelectDropDownValue.java
+++ src/main/java/s_03_assignment/For_14_SelectDropDownValue.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class For_14_ {
+public class For_14_SelectDropDownValue {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
