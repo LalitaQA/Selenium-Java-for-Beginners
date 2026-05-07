69814b77e6cd9978ea693f9e5c9a0cb78eda7ea6
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-07 14:16:05 +0530

    push changes

--- src/test/java/s_06_WebElement_Getter_Methods/WEG_05_GetAriaRole.java
+++ src/test/java/s_06_WebElement_Getter_Methods/WEG_05_GetAriaRole.java
@@ -1,6 +1,8 @@
 package s_06_webelement_getter_methods;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WEG_05_GetAriaRole {
@@ -9,8 +11,8 @@ public class WEG_05_GetAriaRole {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://demowebshop.tricentis.com/");
-		
-		
-	}
 
+		WebElement addToCarbtn = driver.findElement(By.xpath("(//input[@value='Add to cart'])[1]"));
+		System.out.println(addToCarbtn.getAriaRole());
+	}
 }
