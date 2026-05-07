aa9c3cdc65d5dd566a7b96d7e7353509ec156f2d
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-07 16:01:36 +0530

    push changes

---
+++ src/test/java/s_09_dropdown_selectclass/D_01_SelectByVisibleText.java
@@ -0,0 +1,17 @@
+package s_09_dropdown_selectclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class D_01_SelectByVisibleText {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		
+		
+		
+	}
+
+}
