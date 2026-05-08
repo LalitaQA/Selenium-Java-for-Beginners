c61a3c7073b8f51de36d4e0f90ca06873502c0a4
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-08 11:31:14 +0530

    push changes

--- src/test/java/s_06_WebElement_Getter_Methods/WEG_01_GetText.java
+++ src/test/java/s_06_WebElement_Getter_Methods/WEG_01_GetText.java
@@ -1,5 +1,11 @@
 package s_06_webelement_getter_methods;
 
+//1.Launch browser
+//2.Maximize browser
+//3.Navigate to demo web shop website
+//4.Find element "Virtual gift card" using xpath locator
+//5.Get text "Virtual gift card"
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,7 +19,7 @@ public class WEG_01_GetText {
 		driver.get("https://demowebshop.tricentis.com/");
 
 		WebElement register = driver.findElement(By.xpath("(//a[@href='/25-virtual-gift-card'])[2]"));
-		//Get text of web element from web page
+		// Get text of web element from web page
 		System.out.println(register.getText());
 	}
 }
--- src/test/java/s_06_WebElement_Getter_Methods/WEG_02_GetAttribute.java
+++ src/test/java/s_06_WebElement_Getter_Methods/WEG_02_GetAttribute.java
@@ -1,5 +1,11 @@
 package s_06_webelement_getter_methods;
 
+//1.Launch browser
+//2.Maximize browser
+//3.Navigate to demo web shop website
+//4.Find element "Search field" using id locator
+//5.Get attribute using "name"
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,7 +19,7 @@ public class WEG_02_GetAttribute {
 		driver.get("https://demowebshop.tricentis.com/");
 
 		WebElement product = driver.findElement(By.id("small-searchterms"));
-		//Get attribute of web element from web page
+		// Get attribute of web element from web page
 		System.out.println(product.getAttribute("name"));
 	}
 }
