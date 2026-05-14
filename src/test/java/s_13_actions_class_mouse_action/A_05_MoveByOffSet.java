03f2723cba0a80aeee21c0bed6e6db1edd881d8a
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-14 15:01:56 +0530

    push changes

--- src/test/java/s_13_actions_class_mouse_action/A_05_MoveByOffSet.java
+++ src/test/java/s_13_actions_class_mouse_action/A_05_MoveByOffSet.java
@@ -1,10 +1,21 @@
 package s_13_actions_class_mouse_action;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class A_05_MoveByOffSet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+		driver.get("https://demowebshop.tricentis.com/");
+		Actions act = new Actions(driver);
+		act.moveByOffset(0, 0)
 	}
 
 }
