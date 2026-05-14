1312830ddb3f4ad94d830ed4109ffe83a2aa8ef9
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-14 15:49:04 +0530

    push changes

--- src/test/java/s_13_actions_class_mouse_action/A_07_DragAndDropOffSet.java
+++ src/test/java/s_13_actions_class_mouse_action/A_07_DragAndDropOffSet.java
@@ -1,9 +1,20 @@
 package s_13_actions_class_mouse_action;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class A_07_DragAndDropOffSet {
 
 	public static void main(String[] args) {
-		
-	}
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 
+		driver.get("https://www.selenium.dev/selenium/web/mouse_interaction.html");
+		WebElement src = driver.findElement(By.id("draggable"));
+		Actions act = new Actions(driver);
+		act.dragAndDropBy(src, 10, 100).perform();
+	}
 }
---
+++ src/test/java/s_13_actions_class_mouse_action/A_08_.java
@@ -0,0 +1,10 @@
+package s_13_actions_class_mouse_action;
+
+public class A_08_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
