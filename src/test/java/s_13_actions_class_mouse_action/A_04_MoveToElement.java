4a60bc1e9951db33974e41bc460a27d549dd1a1a
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-14 15:00:54 +0530

    push changes

--- src/test/java/s_13_actions_class_mouse_action/A_04_MoveToElement.java
+++ src/test/java/s_13_actions_class_mouse_action/A_04_MoveToElement.java
@@ -1,9 +1,24 @@
 package s_13_actions_class_mouse_action;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class A_04_MoveToElement {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.get("https://www.woodenstreet.com/");
+		Thread.sleep(3000);
+		WebElement profileIcon = driver.findElement(By.xpath("//p[text()='Profile']"));
+		Actions act = new Actions(driver);
+		act.moveToElement(profileIcon);
+
 	}
 
 }
---
+++ src/test/java/s_13_actions_class_mouse_action/A_05_MoveByOffSet.java
@@ -0,0 +1,10 @@
+package s_13_actions_class_mouse_action;
+
+public class A_05_MoveByOffSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
