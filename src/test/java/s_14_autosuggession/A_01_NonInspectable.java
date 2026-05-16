d4832e41e4763e60db2a73aefbe2cdc6cb650264
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-16 14:46:04 +0530

    push changes

--- src/test/java/s_14_autosuggession/A_01_NonInspectable.java
+++ src/test/java/s_14_autosuggession/A_01_NonInspectable.java
@@ -1,9 +1,18 @@
 package s_14_autosuggession;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class A_01_NonInspectable {
 
 	public static void main(String[] args) {
-		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://www.saucedemo.com/");
 	}
 
 }
