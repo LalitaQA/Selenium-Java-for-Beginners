03dea3add2630881587b381353fe31e85fe34203
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-16 14:59:08 +0530

    push changes

--- src/test/java/s_14_autosuggession/A_01_NonInspectable.java
+++ src/test/java/s_14_autosuggession/A_01_NonInspectable.java
@@ -1,8 +1,11 @@
 package s_14_autosuggession;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class A_01_NonInspectable {
@@ -12,7 +15,13 @@ public class A_01_NonInspectable {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		driver.get("https://www.saucedemo.com/");
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.name("q")).sendKeys("iphone");
+		List<WebElement> autolist = driver
+				.findElements(By.xpath("//ul[contains(@class,'VCplLH lTpUwR bRjjIF _1psv1ze5l _1psv1ze9l')]/li"));
+		for (WebElement Autosuggesstion : autolist) {
+			System.out.println(Autosuggesstion.getText());
+		}
 	}
 
 }
