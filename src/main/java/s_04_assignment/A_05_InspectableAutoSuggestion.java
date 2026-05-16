6549f3da22b51157b658f7aed1bc82bbf6f71906
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-16 15:45:05 +0530

    push changes

---
+++ src/main/java/s_04_assignment/A_05_InspectableAutoSuggestion.java
@@ -0,0 +1,9 @@
+package s_04_assignment;
+
+public class A_05_InspectableAutoSuggestion {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/test/java/s_14_autosuggession/AS_02_Inspectable.java
+++ src/test/java/s_14_autosuggession/AS_02_Inspectable.java
@@ -1,9 +1,28 @@
 package s_14_autosuggession;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AS_02_Inspectable {
 
 	public static void main(String[] args) {
-		
-	}
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+		driver.get("https://www.amazon.in/");
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("dress");
+		List<WebElement> autolist = driver.findElements(By.xpath("//div[@role='row']"));
+		for (int i = 0; i < autolist.size(); i++) {
+            String text = autolist.get(i).getText();
+            if (text.toLowerCase().contains("dress")) {
+                System.out.println(text);
+            }
+		}
+	}
 }
