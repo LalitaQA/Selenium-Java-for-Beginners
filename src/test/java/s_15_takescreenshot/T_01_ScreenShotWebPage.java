27faf4b9f5ace7fa508beb463403df5414a4c78b
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-18 15:17:53 +0530

    push changes

--- src/test/java/s_15_takescreenshot/T_01_ScreenShotWebPage.java
+++ src/test/java/s_15_takescreenshot/T_01_ScreenShotWebPage.java
@@ -1,10 +1,26 @@
 package s_15_takescreenshot;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
 public class T_01_ScreenShotWebPage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+		driver.get("https://www.flipkart.com/");
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File tem = ts.getScreenshotAs(OutputType.FILE);
+		File per = new File("./screenshots/ss1.png");
+		FileHandler.copy(tem, per);
 	}
-
 }
