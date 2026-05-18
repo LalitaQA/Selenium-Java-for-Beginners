00f439293146697ff4a12f8c355ff499348cb0b6
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-18 10:45:40 +0530

    push changes

--- src/test/java/s_13_actions_class_mouse_action/A_07_DragAndDropOffSet.java
+++ src/test/java/s_13_actions_class_mouse_action/A_07_DragAndDropOffSet.java
@@ -1,5 +1,13 @@
 package s_13_actions_class_mouse_action;
 
+//1.Launch browser
+//2.Maximize browser
+//3.Use implicit wait
+//4.Navigate to selenium dev website
+//5.Locate draggable element
+//6.Perform drag and drop using x,y value of droppable element
+
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +19,7 @@ public class A_07_DragAndDropOffSet {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		driver.get("https://www.selenium.dev/selenium/web/mouse_interaction.html");
 		WebElement src = driver.findElement(By.id("draggable"));
