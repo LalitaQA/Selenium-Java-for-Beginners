d1a963fc25531affddf871da751c004e9fa08f22
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-18 11:47:40 +0530

    push changes

--- src/test/java/s_14_autosuggession/AS_02_Inspectable.java
+++ src/test/java/s_14_autosuggession/AS_02_Inspectable.java
@@ -3,11 +3,11 @@ package s_14_autosuggession;
 //1.Launch browser
 //2.Maximize browser
 //3.Use implicit wait
-//4.Navigate to flipkart website
+//4.Navigate to amazon website
 //5.Locate search textfield
 //6.Enter value using sendkeys
-//7.Close popup
-//8.Print all autosuggestion text list
+//7.Get all autosuggestion text list
+//8.Print only which have "dress"
 
 import java.time.Duration;
 import java.util.List;
@@ -28,10 +28,10 @@ public class AS_02_Inspectable {
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("dress");
 		List<WebElement> autolist = driver.findElements(By.xpath("//div[@role='row']"));
 		for (int i = 0; i < autolist.size(); i++) {
-            String text = autolist.get(i).getText();
-            if (text.toLowerCase().contains("dress")) {
-                System.out.println(text);
-            }
+			String text = autolist.get(i).getText();
+			if (text.toLowerCase().contains("dress")) {
+				System.out.println(text);
+			}
 		}
 	}
 }
