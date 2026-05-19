ac75fa65982ab2c8569024a39ca362d1380328f2
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-19 15:17:33 +0530

    push changes

--- src/test/java/s_17_javascriptexcutor/JS_02_ScrollToAmount.java
+++ src/test/java/s_17_javascriptexcutor/JS_02_ScrollToAmount.java
@@ -4,24 +4,25 @@ package s_17_javascriptexcutor;
 //2.Maximize browser
 //3.Use implicit wait
 //4.Navigate to amazon
-//5.Scroll the webpage using JavascriptExecutor scrollBy()
+//5.Scroll the webpage using JavascriptExecutor scrollTo()
 
 import java.time.Duration;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class JS_01_ScrollByAmount {
+public class JS_02_ScrollToAmount {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-		driver.get("https://www.amazon.in/");
+		driver.get("https://doodles.google/");
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,200)");
+		js.executeScript("window.scrollTo(0,2000)");
 		Thread.sleep(3000);
-		js.executeScript("window.scrollBy(0,400)");
+		js.executeScript("window.scrollTo(0,4000)");
 	}
+
 }
