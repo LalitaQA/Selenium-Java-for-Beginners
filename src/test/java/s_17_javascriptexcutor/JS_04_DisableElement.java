c0d5e772c7e47fd80f16c549a447284fdfd81f1f
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-20 14:32:39 +0530

    push changes

--- src/test/java/s_17_javascriptexcutor/JS_04_DisableElement.java
+++ src/test/java/s_17_javascriptexcutor/JS_04_DisableElement.java
@@ -2,7 +2,10 @@ package s_17_javascriptexcutor;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JS_04_DisableElement {
@@ -12,7 +15,11 @@ public class JS_04_DisableElement {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-		driver.get("https://www.amazon.in/");
+		driver.get("https://www.oracle.com/java/technologies/javase-jdk26-doc-downloads.html");
+		driver.findElement(By.xpath("//a[text()='jdk-26.0.1_doc-all.zip']")).click();
+		WebElement button = driver.findElement(By.xpath("//a[text()='Download jdk-26.0.1_doc-all.zip']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click()", button);
 	}
 
 }
