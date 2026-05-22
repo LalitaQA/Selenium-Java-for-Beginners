6cfee504e86b9fee022ebba1474f88e8bfb86d16
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-22 15:06:10 +0530

    push changes

--- src/test/java/s_18_popups/P_04_Hidden.java
+++ src/test/java/s_18_popups/P_04_Hidden.java
@@ -1,8 +1,22 @@
 package s_18_popups;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P_04_Hidden {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://www.easemytrip.com/");
+		driver.findElement(By.id("ddate")).click();
+
+		driver.findElement(By.id("fiv_6_30/05/2026")).click();
 		
 	}
 
