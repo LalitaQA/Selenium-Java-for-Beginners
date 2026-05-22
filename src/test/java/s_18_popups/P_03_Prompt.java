ae8d536298c47e117ce1a3622b900cc1894fd888
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-22 14:47:19 +0530

    push changes

--- src/test/java/s_18_popups/P_03_Prompt.java
+++ src/test/java/s_18_popups/P_03_Prompt.java
@@ -1,9 +1,26 @@
 package s_18_popups;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P_03_Prompt {
 
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://demo.guru99.com/test/delete_customer.php");
+
+		driver.findElement(By.name("cusid")).sendKeys("1234");
+		driver.findElement(By.name("submit")).click();
+		Alert a = driver.switchTo().alert();
+		Thread.sleep(2000);
+		a.dismiss();
+		driver.quit();
 	}
 
 }
