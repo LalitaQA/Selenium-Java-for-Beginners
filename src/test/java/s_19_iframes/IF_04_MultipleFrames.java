d43a962aea6a484464842345b4ec963d6d8bba0c
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-28 14:54:12 +0530

    push changes

--- src/test/java/s_19_iframes/IF_04_MultipleFrames.java
+++ src/test/java/s_19_iframes/IF_04_MultipleFrames.java
@@ -1,9 +1,27 @@
 package s_19_iframes;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class IF_04_MultipleFrames {
 
 	public static void main(String[] args) {
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
+		driver.get("https://demoapps.qspiders.com/ui/frames/nested?sublist=1");
+		WebElement frame1 = driver.findElement(By.xpath("//iframe[@class='w-full h-96']"));
+		driver.switchTo().frame(frame1);
+		driver.switchTo().frame(0);
+		driver.findElement(By.id("email")).sendKeys("Admin@gmail.com");
+		driver.switchTo().defaultContent();
+		String Text = driver.findElement(By.xpath("//span[contains(text(),'Write a')]")).getText();
+		System.out.println(Text);
 	}
 
 }
